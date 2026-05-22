78c97107626af2391dc5580abc0ca36596cae899
ynby233 <ynby233@users.noreply.github.com>
2026-05-22 05:45:00 +0000

    Clarify login username label

--- frontend/src/components/LoginScreen.tsx
+++ frontend/src/components/LoginScreen.tsx
@@ -68,11 +68,11 @@ export function LoginScreen({
           initialValues={{ username: '', password: '', totp: '' }}
         >
           <Form.Item
-            label="邮箱"
+            label="用户名"
             name="username"
-            rules={[{ required: true, message: '请输入账号' }]}
+            rules={[{ required: true, message: '请输入用户名' }]}
           >
-            <Input placeholder="使用邮箱地址登录" size="large" />
+            <Input placeholder="使用用户名登录" size="large" />
           </Form.Item>
           <Form.Item
             label="密码"
